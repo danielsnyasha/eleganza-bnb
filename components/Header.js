2c1019927674ed5838a64d589afe21603f956228
Nyasha Musanhu <danielsnyasha@outlook.com>
2023-05-16 15:17:51 +0200

    First Commit

---
+++ components/Header.js
@@ -0,0 +1,90 @@
+import Image from 'next/image'
+import React from 'react'
+import {MenuIcon, SearchIcon, ShoppingCartIcon} from "@heroicons/react/outline"
+
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+function Header() {
+   
+
+  const router = useRouter();
+  const { redirect } = router.query;
+  return (
+    <header className='sticky top-0 z50 shadow-md py-5 md:px-10'>
+
+        {/* Top Nav */}
+        <div className=' items-center bg-amazon_blue p-1 flex-grow py-2 z-50 grid grid-cols-3'>
+            <div className='mt-0 mr-2 flex items-center flex-grow sm:flex-grow-0'>
+                <Image src='/eleganza.jpg' alt='' width={150} height={50} style={{objectFit:"cover"}} className='cursor-pointer rounded-md' />
+            </div>
+            {/* Search: Middle*/}
+        <div className='hidden sm:flex bg-red-400 hover:bg-red-500 items-center h-10 rounded-sm flex-grow cursor-pointer'>
+            <input type="text
+            " className='bg-red-50 p-2 h-full w-6 flex-grow flex-shrink focus:outline-none px-4'/>
+            
+            <SearchIcon className='h-12 p-4'/>
+
+
+        </div>
+
+        {/* Right */}
+        <div className='text-red-50 items-center text-xs font-bold flex space-x-6 mx-6 whitespace-nowrap justify-end '>
+            <Link href='/login'><div className=' link cursor-pointer'>
+                <p className='hover:underline'> Hello Nyasha Daniel </p>
+                <p className='font-extrabold md:text-sm'>Account & Lists</p>
+
+            </div>
+            </Link>
+
+            <div className='link cursor-pointer'>
+                <p>Returns</p>
+                <p className='font-extrabold md:text-sm'>& Orders</p>
+
+            </div>
+
+            <div className='relative link flex items-center'>
+                <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-red-600 text-center rounded-full text-red-50 font-bold'>4
+
+                </span>
+                <ShoppingCartIcon className='h-10'/>
+                <p className='hidden sm:inline font-extrabold mt-2 md:text-sm'>Basket</p>
+            </div>
+
+
+        </div>
+
+
+
+        </div>
+
+        
+
+        {/* Bottom Nav */}
+
+        <div className='flex items-center space-x-3 p-2 bg-gray-800 text-white text-sm shadow-md'>
+            <p className='link flex items-center' >
+                <MenuIcon className='h-6 mr-1'/>
+                All
+            </p>
+            <p className='link'>Rooms</p>
+            <p className='link'>Cabins</p>
+            <p className='link'>Today&apos;s Deals</p>
+
+            <p className='link hidden lg:inline-flex'>Amazing Views</p>
+            <p className='link hidden lg:inline-flex'>Off-the-grid</p>
+            <p className='link hidden lg:inline-flex'>Prime</p>
+            <p className='link hidden lg:inline-flex'>Countryside</p>
+            <p className='link hidden lg:inline-flex'>Africa</p>
+            <p className='link hidden lg:inline-flex'>Europe</p>
+            <p className='link hidden lg:inline-flex'>South America</p>
+
+
+
+        </div>
+    </header>
+  )
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,19 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode:false,
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        amazon_blue: {
+          light: "#a6c0f9",
+          DEFAULT: "#1d4496",
+        },
       },
     },
   },
