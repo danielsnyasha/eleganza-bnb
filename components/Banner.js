44854fb995e4712cc071ca1a09a60448df27a7b6
Nyasha Musanhu <danielsnyasha@outlook.com>
2023-06-05 13:17:37 +0200

    latest-commit

---
+++ components/Banner.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+function Banner() {
+  return (
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+      <Image
+      src='https://cdn.pixabay.com/photo/2016/08/11/23/48/mountains-1587287_1280.jpg'
+      layout='fill'
+      objectFit="cover"
+      loading="eager"
+      />
+
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm sm:text-lg font-bold text-yellow-300">Not sure where to go? Perfect.</p>
+
+        <button className="text-purple-500 active:scale-90 hover:shadow-xl bg-white px-10 py-4 shadow-md rounded-full my-3 font-extrabold "> I'm flexible</button>
+      </div> 
+      
+    </div>
+    
+  );
+}
+
+export default Banner;
---
+++ components/SmallCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from 'next/image';
+
+function SmallCard({ img, location, distance }) {
+  return (
+    <div className="flex items-center mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200">
+      {/* Left */}
+      <div className="relative h-16 w-16 rounded-lg">
+        <Image src={img} alt={location} layout="fill" objectFit="cover" objectPosition="center" className='rounded-lg'/>
+      </div>
+
+      {/* Right */}
+      <div className="ml-4">
+        <h2>{location}</h2>
+        <h3>{distance}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default SmallCard;
