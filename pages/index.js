0a6a17eb986bd3e9f2455002c81e9c7f70b09c88
Nyasha Musanhu <danielsnyasha@outlook.com>
2023-06-05 13:11:59 +0200

    comment new

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,63 @@
+import Banner from "@/components/Banner";
 import Header from "@/components/Header";
+import SmallCard from "@/components/SmallCard";
 import Head from "next/head";
+import  data_  from "../components/data";
 
-export default function Home({products}) {
+
+
+export default function Home({data}) {
   return (
     <div className="bg-gray-100 h-screen">
       <Head>
         <title>Eleganza Shop</title>
       </Head>
 
-     {/* Header */}
-     <Header/>
+      {/* Header */}
+      <Header />
 
-     <main className="max-w-screen-2xl mx-auto">
       {/* Banner */}
-      
 
-      {/* ProductFeed */}
+      <Banner />
 
-    
-      
+      <main className="max-w-screen-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-4 ">
+          <h2 className="text-3 xl font-semibold pb-5">Explore Nearby</h2>
 
-
-     </main>  
-
-     
-
-   
+          {/* Pull some data from a server - API endpoints */}
+         
 
 
 
-    
+          {data_.map(({img, distance, location}) => (
+            <SmallCard key={img} img={img} distance={distance} location={location} />
+  
+))}
+
+          <h3>Test</h3>
+        </section>
+
+        {/* ProductFeed */}
+      </main>
     </div>
   );
 }
+// export async function getStaticProps() {
+//   try {
+//     const exploreData = await fetch('https://links.papareact.com/pyp').then((res) => res.json());
+
+//     return {
+//       props: {
+//         exploreData,
+//       },
+//     };
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+
+//     return {
+//       props: {
+//         exploreData: [],
+//       },
+//     };
+//   }
+// }
