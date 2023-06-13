0609712331b65bb03efa1f26d00e42497f92a49e
Nyasha Musanhu <danielsnyasha@outlook.com>
2023-06-13 08:42:07 +0200

    comm

--- pages/search.js
+++ pages/search.js
@@ -7,16 +7,26 @@ import InfoCard from "@/components/InfoCard";
 import info_data from "@/components/info_data";
 import Map from "@/components/Map";
 
-
 export default function Search({ searchResults }) {
   const router = useRouter();
   const { location, startDate, endDate, noOfGuests } = router.query;
 
-  const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
-  const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
-  const range = `${formattedStartDate} - ${formattedEndDate}`;
+  // ...
+  const startDateObj = startDate ? new Date(startDate) : null;
+  const endDateObj = endDate ? new Date(endDate) : null;
+
+  const formattedStartDate = startDateObj
+    ? format(startDateObj, "dd MMMM yy")
+    : "";
+  const formattedEndDate = endDateObj ? format(endDateObj, "dd MMMM yy") : "";
+  const range =
+    formattedStartDate && formattedEndDate
+      ? `${formattedStartDate} - ${formattedEndDate}`
+      : "";
+  // ...
+
   return (
-    <div className>
+    <div>
       <Header placeholder={`${location} | ${range} | ${noOfGuests}`} />
       <main className="flex">
         <section className="flex-grow pt-10 px-6">
@@ -24,53 +34,42 @@ export default function Search({ searchResults }) {
             300+ Stays - {range} - for {noOfGuests} Guests
           </p>
 
-          <h1 className="text-3xl font-semibold mt-2 mb-6 ">
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
             Stays in {location}
           </h1>
 
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
             <p className="button_">Cancellation Flexibility</p>
-            <p className="button_"> Type of Place </p>
+            <p className="button_">Type of Place</p>
             <p className="button_">Price</p>
             <p className="button_">Rooms and Beds</p>
             <p className="button_">More Filters</p>
           </div>
 
-            <div className="flex flex-col">
-          {info_data &&
-            info_data.map(
-              ({ img, location, title, description, star, price, total }) => (
-                <InfoCard
-                key = {img}
-                  img={img}
-                  location={location}
-                  title={title}
-                  description={description}
-                  star={star}
-                  price={price}
-                  total={total}
-                />
-              )
-            )}
-            </div>
+          <div className="flex flex-col">
+            {info_data &&
+              info_data.map(
+                ({ img, location, title, description, star, price, total }) => (
+                  <InfoCard
+                    key={img}
+                    img={img}
+                    location={location}
+                    title={title}
+                    description={description}
+                    star={star}
+                    price={price}
+                    total={total}
+                  />
+                )
+              )}
+          </div>
         </section>
         <section className="hidden xl:inline-flex md:min-w-[600px]">
-            <Map info_data={info_data}/>
+          <Map info_data={info_data} />
         </section>
       </main>
-
 
       <Footer />
     </div>
   );
 }
-// export async function getServerSideProps() {
-//   const res = await fetch("https://links.papareact.com/isz");
-//   const searchResults = await res.json();
-
-//   return {
-//     props: {
-//       searchResults,
-//     },
-//   };
-// }
